a8c202afe610d7ab557a24a5999c6c9454484ccb
Bertrand Debrie <bd.debrie@gmail.com>
2018-10-16 16:15:26 +0200

    added plugin URI

--- capa-contact.php
+++ capa-contact.php
@@ -2,8 +2,9 @@
 /**
 * Plugin Name: Capa Contact
 * Description: Simple emails
+* Plugin URI: https://github.com/bdebrie/capa-contact
 * Version: 0.1
 * Author: Bertrand DEBRIE
 * Author URI: https://github.com/bdebrie
-* License: none
+* License: n
 */
